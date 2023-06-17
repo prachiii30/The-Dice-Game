ca9c80089c8fc67dc8dc65c7cc4417a71234894c
Prachi Parihar <prachisingh30aabb@gmail.com>
2023-06-17 12:11:44 +0530

    JavaScript Added

--- index.js
+++ index.js
@@ -1,3 +1,13 @@
-var random=98;
-console.log(random);
+var random_no1=Math.floor(Math.random()*6)+1;
+var randomimageDice1="./images/dice"+ random_no1 +".png";
+var image1=document.querySelectorAll("img")[0];
+image1.setAttribute("src",randomimageDice1);
+
+var random_n2=Math.floor(Math.random()*6)+1;
+var randomimageDice2="./images/dice"+random_n2+".png";
+var image2=document.querySelectorAll("img")[1];
+image2.setAttribute("src",randomimageDice2);
+
+
+ 
 
